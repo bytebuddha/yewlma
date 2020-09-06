4af3e1b2cb82abff6f67f0084916c6067b374a91
Bytebuddha <pmg41494@protonmail.com>
2020-09-06 02:38:25 -0500

    add forgotten file from last commit

---
+++ src/components/breadcrumbs.rs
@@ -0,0 +1,61 @@
+use yew::prelude::*;
+use yewtil::NeqAssign;
+use yew_route_breadcrumbs::StaticCrumb;
+use yew_router::prelude::*;
+use yew_router::agent::RouteRequest;
+
+pub struct BreadCrumbs {
+    link: ComponentLink<Self>,
+    props: Props
+}
+
+pub struct Clicked(Option<&'static str>);
+
+#[derive(Debug, PartialEq, Clone, Properties)]
+pub struct Props {
+    pub crumbs: Option<Vec<StaticCrumb>>
+}
+
+impl Component for BreadCrumbs {
+    type Message = Clicked;
+    type Properties = Props;
+
+    fn create(props: Self::Properties, link: ComponentLink<Self>) -> Self {
+        Self { link, props }
+    }
+
+    fn change(&mut self, props: Self::Properties) -> ShouldRender {
+        self.props.neq_assign(props)
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        if let Some(route) = msg.0 {
+            let route = Route::new_no_state(route);
+            RouteAgentDispatcher::new().send(RouteRequest::ChangeRoute(route));
+        }
+        false
+    }
+
+    fn view(&self) -> Html {
+        if let Some(crumbs) = &self.props.crumbs {
+            html! {
+              <nav class="breadcrumb" aria-label="breadcrumbs">
+                <ul>
+                  {
+                      for crumbs.iter().map(|item| {
+                          let route = item.route.clone();
+                          let callback = self.link.callback(move |_| Clicked(route));
+                          html_nested!{
+                              <li><a onclick=callback>{item.text}</a></li>
+                      }})
+                  }
+                </ul>
+              </nav>
+            }
+        } else {
+            html! {
+
+            }
+        }
+    }
+}
